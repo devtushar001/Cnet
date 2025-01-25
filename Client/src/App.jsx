4e851b6f7129e90d47071d96e950b463e4bc9038
devtushar001 <tc341269@gmail.com>
2025-01-25 06:03:55 -0800

    copied come completed files

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Tools from './Pages/Tools/Tools';
+import ToolsView from './Pages/ToolsView/ToolsView';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import PrivacyPolicy from './Pages/PrivacyPolicy/PrivacyPolicy';
+import LoginSignup from './Pages/LoginSignup/LoginSignup';
+import UserProfile from './Pages/UserProfile/UserProfile';
+import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/projects' element={<ToolsView />}/>
+        <Route path="/projects/:toolsId" element={<Tools />} />
+        <Route path='/about-us' element={<AboutUs />}/>
+        <Route path='/contact-us' element={<ContactUs />}/>
+        <Route path='/privacy-policy' element={<PrivacyPolicy />}/>
+        <Route path='/login-signup' element={<LoginSignup />}/>
+        <Route path='/user-profile' element={<UserProfile />}/>
+        <Route path='/forgot-password' element={<ForgotPassword />}/>
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import EscomContextProvider from './Context/escomContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <EscomContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </EscomContextProvider>
+
 )
