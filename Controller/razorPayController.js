c452ef1561014978554151703887fc228ec7ce92
devtushar001 <tc341269@gmail.com>
2025-01-26 21:28:47 -0800

    create payment order and verify payment order completed

--- Controller/razorPayController.js
+++ Controller/razorPayController.js
@@ -1,13 +1,22 @@
-import orders from "razorpay/dist/types/orders.js";
 import { razorPayInstance } from "../Config/razorPayConfig.js";
+import crypto from 'crypto';
+
+const razorPayKeyId = process.env.RAZORPAY_KEY_ID
+const razorPayKeySecret = process.env.RAZORPAY_KEY_SECRET
 
 export const createRazorPayOrderController = async (req, res) => {
+  if (!razorPayKeyId || !razorPayKeySecret) {
+    return res.status(404).json({
+      success: false,
+      message: `jhjg`
+    })
+  }
 
   //Don't get the amount from frontend
   const { courseId, amount } = req.body;
 
   // Create order
-  const rPI = razorPayInstance(process.env.RAZORPAY_KEY_ID, process.env.RAZORPAY_KEY_SECRET);
+  const rPI = razorPayInstance(razorPayKeyId, razorPayKeySecret);
 
   const options = {
     amount: amount * 100,
@@ -21,7 +30,7 @@ export const createRazorPayOrderController = async (req, res) => {
       if (err) {
         return res.status(500).json({
           success: false,
-          message: `Razor pay ${err}`
+          message: `Razorpay create orders ${err}`
         })
       }
       return res.status(200).json(order)
@@ -36,5 +45,19 @@ export const createRazorPayOrderController = async (req, res) => {
 }
 
 export const verifyRazorPayOrderController = async (req, res) => {
-  
+  const { order_id, payment_id, signature } = req.body;
+
+
+  //Create hmac object
+  const hmac = crypto.createHmac("sha256", razorPayKeySecret);
+  hmac.update(order_id + "|" + payment_id);
+
+  const rPS = hmac.digest('hex');
+
+  if (rPS === signature) {
+    return res.status(200).json({
+      success: true,
+      message: `Payment variefied`
+    })
+  }
 }
